a1ceacf3d43c443a46cc56199c1330697e235cf6
Neidschel <Neidschel@gmx.at>
2013-04-20 13:20:21 +0200

    Initial commit (2)

---
+++ OPR2_UE05_NiglM_MC12028/src/at/fhhgb/mc/Aufgabe02/Member.java
@@ -0,0 +1,22 @@
+package at.fhhgb.mc.Aufgabe02;
+
+public abstract class Member {
+
+	public double getIncome(){
+		
+	}
+	
+	public double getCosts(){
+
+	}
+
+	public double getSurplus(){
+	
+	}
+	
+	public String toString(boolean ascending){
+		
+		
+	}
+	
+}
---
+++ OPR2_UE05_NiglM_MC12028/src/at/fhhgb/mc/Aufgabe02/Section.java
@@ -0,0 +1,21 @@
+package at.fhhgb.mc.Aufgabe02;
+
+public class Section {
+	
+	private BinarySearchTree tree;
+	
+	
+	public boolean addMember(Member m){
+		
+		return true;
+	}
+	public boolean removeMember(String name){
+		
+		return true;
+	}
+	public boolean isMember(String name){
+		
+		return true;
+	}
+	
+}
