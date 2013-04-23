aef160c25f1c4a33fc9f2db4af05c6471c8573d3
Neidschel <Neidschel@gmx.at>
2013-04-24 00:33:45 +0200

    toString (finally) working

--- OPR2_UE05_NiglM_MC12028/src/at/fhhgb/mc/Aufgabe02/Member/Section.java
+++ OPR2_UE05_NiglM_MC12028/src/at/fhhgb/mc/Aufgabe02/Member/Section.java
@@ -3,116 +3,168 @@ package at.fhhgb.mc.Aufgabe02.Member;
 import at.fhhgb.mc.Aufgabe02.Abstract.AbstractMember;
 import at.fhhgb.mc.Aufgabe02.Abstract.BinarySearchTree;
 
+public class Section extends AbstractMember {
 
-public class Section extends AbstractMember{
-	
 	private BinarySearchTree tree;
-	
-	public Section(String name){
-		super(name,0,0);
+
+	/**
+	 * The constructor initializes the section with it's name but no income and
+	 * costs because it has no members at this point.
+	 * 
+	 * @param name the name of the new section
+	 */
+	public Section(String name) {
+		super(name, 0, 0);
 	}
 	
-	public boolean addMember(AbstractMember m){
-		if(m == null){
+	
+	public boolean addMember(AbstractMember m) {
+		if (m == null) {
 			return false;
 		}
-		if(tree == null){
+		if (tree == null) {
 			tree = new BinarySearchTree();
 			tree.insert(m);
+			this.setIncome(m.getIncome());
+			this.setCosts(m.getCosts());
 			return true;
-		}else{
+		} else {
 			Comparable[] array = tree.toArray(true);
-			for(int i = 0; i < array.length;i++){
+			for (int i = 0; i < array.length; i++) {
 				if (array instanceof AbstractMember[]) {
-					if( ((AbstractMember) array[i]).getName().equals(m.getName())){
+					if (((AbstractMember) array[i]).getName().equals(
+							m.getName())) {
 						return false;
 					}
-					
+
 				}
-					
+
 			}
-			
+			this.setIncome(m.getIncome());
+			this.setCosts(m.getCosts());
 			tree.insert(m);
 			return true;
 		}
 	}
-	
-	
-	public boolean removeMember(String name){
-		
-		if(tree == null || name == null){
+
+	/**
+	 * 
+	 */
+	public void setIncome(double income) {
+		double result = getIncome() + income;
+		super.setIncome(result);
+	}
+
+	public void setCosts(double costs) {
+		double result = getCosts() + costs;
+		super.setCosts(result);
+	}
+
+	public boolean removeMember(String name) {
+
+		if (tree == null || name == null) {
 			return false;
-		}else{
+		} else {
 			Comparable[] array = tree.toArray(true);
 			boolean check = false;
 			int i = 0;
-			
-			while(i < array.length && check == false){
+
+			while (i < array.length && check == false) {
 				if (array instanceof AbstractMember[]) {
-					if( ((AbstractMember) array[i]).getName().equals(name)){
+					if (((AbstractMember) array[i]).getName().equals(name)) {
 						check = true;
 						i--;
 					}
 				}
 				i++;
 			}
-			
-			if(check==false){
+
+			if (check == false) {
 				return false;
-			}else{
-				Comparable[] newTree = new Comparable[array.length-1];
-				for(int j = 0; j < newTree.length;j++){
-					if(j<i){
+			} else {
+				Comparable[] newTree = new Comparable[array.length - 1];
+				for (int j = 0; j < newTree.length; j++) {
+					if (j < i) {
 						newTree[j] = array[j];
-					}else{
-						newTree[j] = array[j+1];
+					} else {
+						newTree[j] = array[j + 1];
 					}
 				}
-				
 				tree.balance(newTree);
 				return true;
-				
 			}
-			
 		}
 	}
-	
-	public boolean isMember(String name){
-		if(tree == null || name == null){
+
+	public boolean isMember(String name) {
+		if (tree == null || name == null) {
 			return false;
-		}else{
+		} else {
 			Comparable[] array = tree.toArray(true);
-			
-			for(int i = 0; i < array.length;i++){
+
+			for (int i = 0; i < array.length; i++) {
 				if (array instanceof AbstractMember[]) {
-					if( ((AbstractMember) array[i]).getName().equals(name)){
+					if (((AbstractMember) array[i]).getName().equals(name)) {
 						return true;
 					}
 				}
 			}
-			
+
 			return false;
 		}
 	}
-	
-	public String toString(boolean ascending){
-		Comparable[] array;
+
+	public String toString(boolean ascending) {
 		StringBuilder output = new StringBuilder();
-		if(ascending == true){
-			array = tree.toArray(true);
-		}else{
-			array = tree.toArray(false);
-		}
-		
-		output.append(" "+"The Club "+this.getName()+" earns the amount of: "+this.getSurplus()+"\n");
-		output.append(" "+"It has the following Members: "+"\n");
-		
-		for(int i = 0; i < array.length;i++){
-			output.append(" "+((AbstractMember) array[i]).toString(true)+"\n");
-		}
+		toString(output, ascending, this, 0);
 
 		return output.toString();
-		
 	}
-	
+
+	private int toString(StringBuilder output, boolean ascending, Section s,
+			int offset) {
+		Comparable[] array;
+		// StringBuilder output = new StringBuilder();
+		if (ascending == true) {
+			array = s.tree.toArray(true);
+		} else {
+			array = s.tree.toArray(false);
+		}
+		StringBuilder tabulator = new StringBuilder();
+		for (int i = 0; i < offset; i++) {
+			tabulator.append("\t");
+		}
+		output.append(tabulator + "---------------------------------------"
+				+ "\n");
+		output.append(tabulator + "---------------------------------------"
+				+ "\n");
+		output.append(tabulator + s.getName() + " Club " + "\n");
+		output.append(tabulator + "- has an income of: " + s.getIncome() + "\n");
+		output.append(tabulator + "- has costs of: " + s.getCosts() + "\n");
+		output.append(tabulator + "- earns: " + s.getSurplus() + " a year."
+				+ "\n");
+		output.append(tabulator + "- has the following Members: " + "\n");
+		output.append(tabulator + "---------------------------------------"
+				+ "\n");
+		output.append(tabulator + "---------------------------------------"
+				+ "\n");
+		for (int i = 0; i < array.length; i++) {
+			if (((AbstractMember) array[i]) instanceof Section) {
+				offset += 1;
+				toString(output, ascending, ((Section) array[i]), offset);
+			} else {
+				output.append(tabulator + "\t"
+						+ ((AbstractMember) array[i]).toString(ascending)
+						+ "\n");
+			}
+		}
+		output.append(tabulator + "---------------------------------------"
+				+ "\n");
+		output.append(tabulator + "---------------------------------------"
+				+ "\n");
+		offset--;
+
+		return offset;
+
+	}
 }
--- OPR2_UE05_NiglM_MC12028/test/at/fhhgb/mc/Aufgabe02test/TrainerTest.java
+++ OPR2_UE05_NiglM_MC12028/test/at/fhhgb/mc/Aufgabe02test/TrainerTest.java
@@ -22,13 +22,23 @@ public class TrainerTest {
 
 		Trainer person = new Trainer("Sepp Maier",5);
 		TopAthlete person2 = new TopAthlete("Kunigunde",5);
-		AmateurAthlete person3 = new AmateurAthlete("Sepp Maier",5);
+		AmateurAthlete person3 = new AmateurAthlete("Ruediger",5);
 		Section section1 = new Section("VolleyBall");
 		section1.addMember(person);
 		section1.addMember(person2);
 		section1.addMember(person3);
 		
-		System.out.println(section1.toString(true));
+		Trainer person4 = new Trainer("Ralph Maurer",7);
+		TopAthlete person5 = new TopAthlete("Lila Blau",2);
+		AmateurAthlete person6 = new AmateurAthlete("GUENTAAA gueni",3);
+		Section section2 = new Section("Tischtennis");
+		section2.addMember(person4);
+		section2.addMember(person5);
+		section2.addMember(person6);
+		
+		section1.addMember(section2);
+		
+		System.out.println(section1.toString(false));
 	}
 
 	
