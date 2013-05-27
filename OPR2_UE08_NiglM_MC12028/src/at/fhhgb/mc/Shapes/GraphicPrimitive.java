87c1941dbb5d8629566fe2e8cd64487acb832a0e
Neidschel <Neidschel@gmx.at>
2013-05-27 11:50:42 +0200

    backup all working

--- OPR2_UE08_NiglM_MC12028/src/at/fhhgb/mc/Shapes/GraphicPrimitive.java
+++ OPR2_UE08_NiglM_MC12028/src/at/fhhgb/mc/Shapes/GraphicPrimitive.java
@@ -3,38 +3,64 @@ package at.fhhgb.mc.Shapes;
 import java.awt.*;
 import java.awt.Rectangle;
 
-
-
-
-public abstract class GraphicPrimitive{
+public abstract class GraphicPrimitive {
 	
+	//checks if the object is selected
 	private boolean selected = false;
+	//checks if the object is hovered
 	private boolean hovered = false;
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	public abstract String toString();
-	
+
+	/**
+	 * Generates a Bounding Box of a GraphicPrimitive Object.
+	 * 
+	 * @return the Bounding Box Rectangle of this object.
+	 */
 	public abstract Rectangle getBoundingBox();
 
+	/**
+	 * Moves an GraphicPrimitive object.
+	 * 
+	 * @param dx
+	 *            position change for the x coordinate
+	 * @param dy
+	 *            position change for the y coordinate
+	 */
 	public abstract void move(int dx, int dy);
-	
+
+	/**
+	 * Draws this object on the DrawPanel.
+	 * 
+	 * @param g
+	 *            the Graphic Container
+	 */
 	public abstract void draw(Graphics g);
-	
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#clone()
+	 */
 	public abstract GraphicPrimitive clone();
-	
-	public void setSelected(boolean selected){
+
+	public void setSelected(boolean selected) {
 		this.selected = selected;
 	}
-	
-	public boolean getSelected(){
+
+	public boolean getSelected() {
 		return selected;
 	}
-	
-	public void setHovered(boolean hovered){
+
+	public void setHovered(boolean hovered) {
 		this.hovered = hovered;
 	}
-	
-	public boolean getHovered(){
+
+	public boolean getHovered() {
 		return hovered;
 	}
-	
+
 }
