16c7b3adeb2054b78a55fee533747d35ffc6ce34
Neidschel <Neidschel@gmx.at>
2013-06-29 14:22:57 +0200

    moved Ue10

--- dev/OPR2_UE06_NiglM_MC12028/src/at/fhhgb/mc/Aufgabe01/InvalidAccessException.java
+++
@@ -1,8 +0,0 @@
-package at.fhhgb.mc.Aufgabe01;
-
-public class InvalidAccessException extends Exception{
-	
-	public InvalidAccessException(String _msg) {
-		super(_msg);
-	}
-}
--- dev/OPR2_UE06_NiglM_MC12028/src/at/fhhgb/mc/Aufgabe01/ValueException.java
+++
@@ -1,9 +0,0 @@
-package at.fhhgb.mc.Aufgabe01;
-
-public class ValueException extends Exception{
-	
-	public ValueException (String _msg, int _whichVal){
-		super(_msg+" The Exception was thrown while the value "+_whichVal+" was used!");
-	}
-
-}
